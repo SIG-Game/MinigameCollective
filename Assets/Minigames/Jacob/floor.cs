402dcabc37fc10fd321ef2467c5bffa36945df57
CJacob314 <jacob@jacobcohen.info>
2021-06-11 19:40:35 -0500

    Added a character and background
    Also added movement via the arrow keys or WASD (only supports left and
    right).
    
    Gravity and a floor (without collider just a weird script). I will fix
    that later.

---
+++ Assets/Minigames/Jacob/floor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor : MonoBehaviour
+{
+    bool isLocked = false;
+    public float heightToFreeze = -4.5f;
+    public float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= heightToFreeze) isLocked = true;
+
+        Vector3 now = transform.position;
+        now.y = heightToFreeze;
+        if (isLocked) transform.position = now;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(horizontal * movementSpeed * Time.deltaTime, 0, 0);
+    }
+}
