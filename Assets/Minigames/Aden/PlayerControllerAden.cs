2bdad84ead40012a7be6b477a1e1df95e9b3f337
adenprince <45126168+adenprince@users.noreply.github.com>
2021-06-24 22:24:50 -0500

    Added ground and player with horizontal movement and jumping

---
+++ Assets/Minigames/Aden/PlayerControllerAden.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerAden : MonoBehaviour
+{
+    public float xSpeed;
+    public float jumpSpeed;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVelocity = xSpeed * Input.GetAxisRaw("Horizontal");
+        float yVelocity = rb2d.velocity.y;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            yVelocity = jumpSpeed;
+        }
+
+        rb2d.velocity = new Vector2(xVelocity, yVelocity);
+    }
+}
