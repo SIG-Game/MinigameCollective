e1ca25e65dedc37b120864cf270d5d9f5de56d29
adenprince <45126168+adenprince@users.noreply.github.com>
2021-06-24 23:39:16 -0500

    Added shooting mechanic

--- Assets/Minigames/Aden/PlayerControllerAden.cs
+++ Assets/Minigames/Aden/PlayerControllerAden.cs
@@ -7,7 +7,11 @@ public class PlayerControllerAden : MonoBehaviour
     public float xSpeed;
     public float jumpSpeed;
 
+    public GameObject bullet;
+    public float bulletSpeed;
+
     Rigidbody2D rb2d;
+    int direction = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +25,31 @@ public class PlayerControllerAden : MonoBehaviour
         float xVelocity = xSpeed * Input.GetAxisRaw("Horizontal");
         float yVelocity = rb2d.velocity.y;
 
+        if (xVelocity != 0.0f && direction != Mathf.Sign(xVelocity)) {
+            direction *= -1;
+            Vector3 newLocalScale = new Vector3(direction, 1.0f, 1.0f);
+            transform.localScale = newLocalScale;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             yVelocity = jumpSpeed;
         }
 
         rb2d.velocity = new Vector2(xVelocity, yVelocity);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 bulletPos = transform.position;
+            bulletPos.x += direction;
+
+            GameObject spawnedBullet = Instantiate(bullet, bulletPos, Quaternion.identity);
+
+            Vector3 bulletLocalScale = spawnedBullet.transform.localScale;
+            bulletLocalScale.x *= direction;
+
+            spawnedBullet.transform.localScale = bulletLocalScale;
+            spawnedBullet.GetComponent<Rigidbody2D>().velocity = new Vector3(direction * bulletSpeed, 0.0f, 0.0f);
+        }
     }
 }
