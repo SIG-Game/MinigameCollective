39816f4e7cf76c9a2a3704510f6ec5fb0406a954
emilygc51 <73391555+emilygc51@users.noreply.github.com>
2021-07-09 19:02:47 -0500

    Added Coin Audio and created new scenes

--- Assets/Minigames/Emily/CoinCollectorEmily.cs
+++ Assets/Minigames/Emily/CoinCollectorEmily.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class CoinCollectorEmily : MonoBehaviour
 {
     public int coinValue = 1;
+    AudioSource audio;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("CoinEmily"))
         {
+            audio = GetComponent<AudioSource>();
+            audio.Play();
             Destroy(other.gameObject);
             ScoreManagerEmily.instance.ChangeScore(coinValue);
         }
