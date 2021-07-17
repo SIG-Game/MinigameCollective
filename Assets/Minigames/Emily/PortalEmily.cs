636bf365250b51f8d9d31a1bcfe324a9136ee106
emilygc51 <73391555+emilygc51@users.noreply.github.com>
2021-07-16 19:38:57 -0500

    Created portal script and began designing level 2

---
+++ Assets/Minigames/Emily/PortalEmily.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalEmily : MonoBehaviour
+{
+    public string LevelName;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("Huh");
+            SceneManager.LoadScene(LevelName);
+        }
+    }
+}
