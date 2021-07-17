04c4568c42b08fbe0aabf0b9e12148e0ff101108
adenprince <45126168+adenprince@users.noreply.github.com>
2021-07-16 23:18:40 -0500

    Added switch activated by bullets

---
+++ Assets/Minigames/Aden/SwitchAden.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchAden : MonoBehaviour
+{
+    public enum SwitchState { On, Off }
+
+    public UnityEvent whenTurnedOn;
+    public UnityEvent whenTurnedOff;
+
+    SwitchState curState;
+    Animator anim;
+
+    private void Start()
+    {
+        curState = SwitchState.Off;
+
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            if (curState == SwitchState.On)
+            {
+                anim.SetBool("IsOn", false);
+                curState = SwitchState.Off;
+                whenTurnedOff.Invoke();
+            }
+            else
+            {
+                anim.SetBool("IsOn", true);
+                curState = SwitchState.On;
+                whenTurnedOn.Invoke();
+            }
+        }
+    }
+}
