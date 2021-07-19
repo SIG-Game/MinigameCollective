b38c4439b0a456039592c48607e22667801d8340
emilygc51 <73391555+emilygc51@users.noreply.github.com>
2021-07-19 01:28:17 -0500

    Created Start Screen

---
+++ Assets/Minigames/Emily/StartScreenAnimationEmily.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenAnimationEmily : MonoBehaviour
+{
+    public float movementSpeed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += -transform.right * movementSpeed * Time.deltaTime;
+        if (transform.position.x >= 12) {
+            transform.position = new Vector3(-12, transform.position.y, transform.position.z);
+        }
+    }
+}
