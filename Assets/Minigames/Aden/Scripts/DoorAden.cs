042b4a803359a4a652245f1fc19bae13509c54aa
adenprince <45126168+adenprince@users.noreply.github.com>
2021-08-27 23:05:05 -0500

    Added doors for getting between levels

---
+++ Assets/Minigames/Aden/Scripts/DoorAden.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorAden : MonoBehaviour
+{
+    public string destinationScene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            SceneManager.LoadScene(destinationScene);
+        }
+    }
+}
